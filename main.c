84f73afa06fc0518949631b2b5bd348975cb368f
JeethanTauro <tauroshanjeeth@gmail.com>
2026-02-22 10:12:14 +0530

    creating the initial folders

---
+++ main.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <errno.h>
+#include <dirent.h>
+#include <string.h>
+#include <sys/stat.h>
+
+void createJitDir() {
+    DIR* dir = opendir("./.jit");
+
+   //if the directory exists, then close
+    if(dir){
+        printf("\n Already Initialised");
+        closedir(dir);
+    }
+    //if the directory doesn't exist
+    else if(ENOENT == errno){
+        const char* headDir="./.jit/refs/heads";
+        const char* refsDir="./.jit/refs";
+        const char* objectsDir="./.jit/objects";
+        const char* rootDir = "./.jit";
+
+
+        if (mkdir(rootDir,0755) == 0) {
+            printf("\n Jit root directory created");
+        }
+        if (mkdir(objectsDir,0755) == 0) {
+            printf("\n objects directory created");
+        }
+        if (mkdir(refsDir,0755) == 0) {
+            printf("\n refs directory created");
+        }
+        if (mkdir(headDir,0755) == 0) {
+            printf("\n head directory created");
+
+            //creating a HEAD file
+            FILE* file_ptr = fopen("./.jit/refs/heads/HEAD","w");
+            if(file_ptr == NULL) {
+                perror("\n failed to create file");
+                return;
+            }
+            char str[] = "ref: refs/heads/master";
+
+            //writing to the HEAD file
+            fprintf(file_ptr,"%s",str);
+            fclose(file_ptr);
+            printf("\n HEAD file created");
+        }
+        //creating the index file
+        FILE* file_ptr = fopen("./.jit/index","w");
+        if(file_ptr == NULL) {
+            perror("\n failed to create file");
+            return;
+        }
+        fclose(file_ptr);
+        printf("\n index file created");
+
+    }
+    else {
+        perror("\n Error creating directory");
+    }
+
+}
+int main(int argc, char* argv[]) {
+    //argv[0] is always the file name
+    //argv[1] I want it to be the command jit
+    //argv[2] I want it to be the command init
+    if (strcmp(argv[1],"init")==0) {
+        createJitDir();
+        printf("\n Jit initialised created \n");
+    }
+    else {
+        perror("SYNTAX : filename jit init");
+    }
+
+    return 0;
+}
+
+
+/*
+ Functions used here in this file:
+
+ */
